f30ba10a9a53be9490fd1ac27e3fad0d2d2239f1
Hackbright Student <info@hackbrightacademy.com>
2015-10-21 18:01:52 -0700

    added login page, templates, finishing submit login route

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,7 +22,41 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/login")
+def log_in():
+    """Show user login."""
+
+
+    return render_template("login.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    """Show user login."""
+    email = request.form["email"]
+    password = request.form["password"]
+
+    # We need to check db to see if email and password exist, otherwise add it to db
+
+    user = User.query.filter((User.email==email) & (User.password == password).first()
+
+    db.session.add(user, password)
+    db.session.commit()
+
+    print user
+
+    return redirect("/")
+
 
 
 if __name__ == "__main__":
